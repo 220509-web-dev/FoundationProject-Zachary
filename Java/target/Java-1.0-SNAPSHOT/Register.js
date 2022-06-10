9c321944ef1b0ac860993c0436bf02e6d28289ba
ZacharyCooremans <77285580+ZacharyCooremans@users.noreply.github.com>
2022-06-10 02:10:57 -0500

    register works with radio

--- Java/target/Java-1.0-SNAPSHOT/Register.js
+++ Java/target/Java-1.0-SNAPSHOT/Register.js
@@ -8,15 +8,28 @@ function Register() {
     let email = document.getElementById("email").value
     let username = document.getElementById("username").value
     let password = document.getElementById("password").value
-    let role_id = document.getElementById("role_id").value
     let department_id = document.getElementById("department_id").value
+
+    let ex = []
+
+    let radio = document.getElementsByName('role_id')
+
+            for (i = 0; i < radio.length; i++) {
+                if(radio[i].checked){
+                    let test = parseInt(radio[i].value)
+                    ex.push(test)
+                }
+            }
+    let role_id = ex[0];
+
 
     fetch('/FoundationProject/users', {
         method: "POST",
         body: JSON.stringify({first_name, last_name, email, username, password, role_id, department_id})
     }) .then(res => {
+    console.log(res)
         console.log("Registered")
-        window.location.href = 'index.html'
+        //window.location.href = 'index.html'
     }) .catch (err => {
         console.log("FAILED")
     })
